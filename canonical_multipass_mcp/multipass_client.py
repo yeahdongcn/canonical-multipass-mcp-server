b204c7d32c7085ca56ab7409b24da06d0209fffd
Xiaodong Ye <yeahdongcn@gmail.com>
2025-07-17 10:01:44 +0800

    2
    Signed-off-by: Xiaodong Ye <yeahdongcn@gmail.com>

--- canonical_multipass_mcp/multipass_client.py
+++ canonical_multipass_mcp/multipass_client.py
@@ -12,7 +12,14 @@ class MultipassClient:
         self._channel = grpc.insecure_channel(self._server_addr)
         self._stub = RpcStub(self._channel)
 
-    def list(self):
-        return self._stub.list(ListRequest())
+    def list(self, verbosity=0, snapshots=False, request_ipv4=False):
+        def request_iterator():
+            yield ListRequest(
+                verbosity_level=verbosity,
+                snapshots=snapshots,
+                request_ipv4=request_ipv4
+            )
+        for resp in self._stub.list(request_iterator()):
+            yield resp
 
 
--- canonical_multipass_mcp/server.py
+++ canonical_multipass_mcp/server.py
@@ -3,8 +3,8 @@ from .multipass_client import MultipassClient
 
 def main():
     client = MultipassClient(server_addr="unix:///var/run/multipass_socket")
-    response = client.list()
-    print(response)
+    for reply in client.list():
+        print(reply)
 
 if __name__ == "__main__":
     main()
